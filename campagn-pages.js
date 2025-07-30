8e24359417a4b66a40f0a30a8a0ee9742af68886
Vladimir Zharinov <feedree@gmail.com>
2025-07-30 15:39:05 +0700

    css дщфв

--- campagn-pages.js
+++ campagn-pages.js
@@ -1,1 +1,8 @@
 console.log("Подключен скрипт для сборов");
+
+(function () {
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = "https://feedree.github.io/aist-scripts/campagn-pages.css";
+  document.head.appendChild(link);
+})();
